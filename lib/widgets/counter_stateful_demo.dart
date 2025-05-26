3a17a1508e1324f2cf1d9b10ac65bf70d0d09fd2
BadonDuke <badonduke@gmail.com>
2025-05-26 21:59:47 +0800

    widget variables vs state variables

--- lib/widgets/counter_stateful_demo.dart
+++ lib/widgets/counter_stateful_demo.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CounterStateful extends StatefulWidget {
-  const CounterStateful({super.key});
+  Color buttonColor;
+  CounterStateful({super.key, required this.buttonColor});
 
   @override
   State<CounterStateful> createState() { 
@@ -12,6 +13,7 @@ class CounterStateful extends StatefulWidget {
 
 class _CounterStatefulState extends State<CounterStateful> {
   int counter = 0;
+
 
   void increment(){
     setState(() {
@@ -24,6 +26,7 @@ class _CounterStatefulState extends State<CounterStateful> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
+        backgroundColor: widget.buttonColor,
         child: Icon(Icons.add),
         onPressed: (){
           increment();
