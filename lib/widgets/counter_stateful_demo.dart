b4bb1000bfb49fc625eceb5a5d6101745cc74931
BadonDuke <badonduke@gmail.com>
2025-05-26 22:37:14 +0800

    lifecycle of a stateful widget

--- lib/widgets/counter_stateful_demo.dart
+++ lib/widgets/counter_stateful_demo.dart
@@ -5,10 +5,7 @@ class CounterStateful extends StatefulWidget {
   CounterStateful({super.key, required this.buttonColor});
 
   @override
-  State<CounterStateful> createState() { 
-    State<CounterStateful> stateClassAssociatedWithThisWidget = _CounterStatefulState();
-    return stateClassAssociatedWithThisWidget;
-  }
+State<CounterStateful> createState() => _CounterStatefulState();
 }
 
 class _CounterStatefulState extends State<CounterStateful> {
@@ -16,15 +13,30 @@ class _CounterStatefulState extends State<CounterStateful> {
 
 
   void increment(){
+    if (mounted){
     setState(() {
       counter++;
     });
-    counter++;
+    }
     print(counter);
   }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Counter'),),
       floatingActionButton: FloatingActionButton(
         backgroundColor: widget.buttonColor,
         child: Icon(Icons.add),
