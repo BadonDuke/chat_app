589621ac0aef828e924f788861bc50dcb9ed14c3
BadonDuke <badonduke@gmail.com>
2025-05-27 18:39:42 +0800

    organizing the styles and colors for the chat page

--- lib/Login_Page.dart
+++ lib/Login_Page.dart
@@ -1,4 +1,5 @@
 import "package:chat_app/Chat_Page.dart";
+import "package:chat_app/utils/spaces.dart";
 import "package:chat_app/utils/textfield_styles.dart";
 import "package:chat_app/widgets/login_text_field.dart";
 import "package:flutter/material.dart";
@@ -64,10 +65,8 @@ final passwordController = TextEditingController();
                         },
                         controller: userNameController,
                       ),
-                      SizedBox(
-                        height: 24,
-                      ),
-                     
+                      verticalSpacing(24),
+
                       LoginTextField(
                         hasAsterisks: true,
                         controller: passwordController, 
@@ -76,9 +75,7 @@ final passwordController = TextEditingController();
                     ],
                   ),
                 ),
-                    SizedBox(
-                        height: 24,
-                      ),
+                   verticalSpacing(24),
                 ElevatedButton(onPressed: (){
                   loginUser(context);
                 },
---
+++ lib/utils/brand_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class BrandColor{
+  static Color ChatInputColor = const Color(0xff1d1c21);
+  static Color primaryColor = Colors.deepPurple;
+}
