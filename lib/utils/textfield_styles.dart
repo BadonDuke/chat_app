05045a73b313ad94f78f90fb0efb740b8f794420
BadonDuke <badonduke@gmail.com>
2025-05-27 21:44:34 +0800

    Exploring pub.dev

--- lib/utils/textfield_styles.dart
+++ lib/utils/textfield_styles.dart
@@ -1,5 +1,6 @@
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 class ThemeTextStyle{
-  static TextStyle loginTextFieldStlye =  TextStyle(color: Colors.blueGrey);
+  static TextStyle loginTextFieldStlye =  GoogleFonts.lato(textStyle: TextStyle(color: Colors.blueGrey));
 }
