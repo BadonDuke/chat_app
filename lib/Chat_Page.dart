112aade473f142824298497f2268ac2f640ac5a8
BadonDuke <badonduke@gmail.com>
2025-05-29 08:50:23 +0800

    futurebuilder for futures

--- lib/Chat_Page.dart
+++ lib/Chat_Page.dart
@@ -39,7 +39,7 @@ onMessageSent(ChatMessageEntity entity) {
     setState(() {});
   }
 
- _getNetworkImages() async {
+ Future<List<PixelfordImage>>_getNetworkImages() async {
     var endpointUrl = Uri.parse('https://picsum.photos/v2/list');
 
     final response = await http.get(endpointUrl);
@@ -51,6 +51,9 @@ onMessageSent(ChatMessageEntity entity) {
         return PixelfordImage.fromJson(listItem);
       }).toList();
       print(_imageList[0].urlFullSize);
+       return _imageList;
+    } else {
+      throw Exception('API not successful!');
     }
   }
   
@@ -80,6 +83,14 @@ onMessageSent(ChatMessageEntity entity) {
       ),
       body: Column(
         children: [
+            FutureBuilder<List<PixelfordImage>>(
+              future: _getNetworkImages(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<PixelfordImage>> snapshot) {
+                if (snapshot.hasData)
+                  return Image.network(snapshot.data![0].urlFullSize);
+                return CircularProgressIndicator();
+              }),
           Expanded(
             child: ListView.builder(
               itemCount: _messages.length,
